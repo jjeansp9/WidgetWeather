35364c5cbf8b8760844dbe010f98be85a244220c
박진솔 <jjeansp9@gmail.com>
2023-03-02 18:05:40 +0900

    주간 기온, 요일 데이터 불러오기 기능 추가 // 가져온 위도,경도를 주소로 변환 // 하늘상태에 따라 이미지, 텍스트 변경 // 위젯 xml 작업, 버튼클릭 작업

--- app/src/main/java/kr/co/widgetweather/WidgetProject.java
+++ app/src/main/java/kr/co/widgetweather/WidgetProject.java
@@ -1,21 +1,34 @@
 package kr.co.widgetweather;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.RemoteViews;
+
+import kr.co.widgetweather.activities.MainActivity;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class WidgetProject extends AppWidgetProvider {
 
+    private final String ACTION_BTN = "ButtonClick";
+
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_project);
+
+        Intent intent = new Intent(context, WidgetProject.class).setAction(ACTION_BTN);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,0, intent, 0);
+        remoteViews.setOnClickPendingIntent(R.id.previous, pendingIntent);
+
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
     }
 
     @Override
@@ -31,12 +44,30 @@ public class WidgetProject extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
             int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_project);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+//        CharSequence widgetText = context.getString(R.string.appwidget_text);
+//        // Construct the RemoteViews object
+//        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_project);
+//        views.setTextViewText(R.id.appwidget_text, widgetText);
+//
+//        // Instruct the widget manager to update the widget
+//        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
 
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        String action = intent.getAction();
+        if (action.equals(ACTION_BTN)){
+            //버튼 클릭 결과를 로그로 확인합니다.
+            Log.d("이벤트클릭 테스트 ","클릭!");
+            //버튼 클릭 결과를 위젯 위의 텍스트뷰를 변경함으로 확인합니다.
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_project);
+            ComponentName componentName = new ComponentName(context, WidgetProject.class);
+
+            remoteViews.setTextViewText(R.id.previous,"이벤트발생!");
+            appWidgetManager.updateAppWidget(componentName, remoteViews);
+        }
     }
 }
